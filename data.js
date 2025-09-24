076466c82871ac60fae92d87d87ae9bf047230be
kimbyeongseok <tmxlraos@gmail.com>
2025-09-24 16:21:51 +0900

    update

--- data.js
+++ data.js
@@ -2,6 +2,26 @@ window.contents = [
 
     //**********/ Art - Works
     {
+        id: "7718",
+        nav: "art",
+        subnav: "works",
+        img: "img/7718_03.jpg",
+        title: "7718",
+        subtitle: "2024<br/>합판 위에 유채, 캔버스 천<br/>가변 크기",
+        body: "1) 새벽 3시경 침대에 누우면 보이는 불 켜진 창문<br/>2) 안토넬로 다 메시나의 그림 〈Saint Jerome in His Study〉(1474) 속에 보이는 창문<br/>3) 요셉 니세포르 니엡스의 사진 〈그라의 창문에서 바라본 조망〉(1826)을 따라 그린 그림<br/>4) 소설들에 나타난 닫힌 얇은 장지문<br/>5) 알베르토 자코메티가 자신의 작업실 창문에서 바라본 풍경을 그린 풍경화 〈Dark landscape (Stampa)〉(1952)를 따라 그린 그림<br/>6-1) 아침에 누군가의 창문에 비치는 산<br/>6-2) 새벽 4시경 시계만 보이는 누군가의 창문 ",
+        images: ["img/7718_01.jpg", "img/7718_02.jpg", "img/7718_03.jpg", "img/7718_04.jpg", "img/7718_05.jpg", "img/7718_06.jpg", "img/7718_07.jpg", "img/7718_08.jpg", "img/7718_09.jpg", "img/7718_10.jpg", "img/7718_11.jpg", "img/7718_12.jpg"]
+    },
+    {
+        id: "bluecurtain",
+        nav: "art",
+        subnav: "works",
+        img: "img/bluecurtain_01.jpg",
+        title: "파랑 커튼",
+        subtitle: "2024<br/>합판 위에 유채, 콩테<br/>240 × 74.8 × 14.4cm",
+        body: "",
+        images: ["img/bluecurtain_01.jpg", "img/bluecurtain_02.jpg"]
+    },
+    {
         id: "howling",
         nav: "art",
         subnav: "works",
@@ -12,14 +32,14 @@ window.contents = [
         images: ["img/img01.png", "img/img02.png", "img/img03.png"]
     },
     {
-        id: "park",
+        id: "carved_flag",
         nav: "art",
         subnav: "works",
-        img: "img/img02.png",
-        title: "Park",
-        subtitle: "solo exhibition<br/>2023.9.8 – 24<br/>RAINBOW CUBE",
+        img: "img/carved_flag_01.png",
+        title: "Carved flag",
+        subtitle: "no. 14. flag<br/>176mm*225mm, canvas fabric on canvas fabric<br/>original/light",
         body: "설명 텍스트가 여기에 들어갑니다.",
-        images: ["img/img02.png"]
+        images: ["img/carved_flag_01.png"]
     },
     {
         id: "park2",
@@ -64,21 +84,21 @@ window.contents = [
 
     //**********/ Art - Drawing //
     {
-        id: "drawing1",
+        id: "movement collection",
         nav: "art",
         subnav: "drawing",
-        img: "img/img04.png",
-        title: "Drawing 1",
-        subtitle: "",
+        img: "img/movement_01.png",
+        title: "movement collection",
+        subtitle: "2018<br/>choreography for non dance<br/>paper and video",
         body: "작품 설명을 여기에 입력하세요.",
-        images: ["drawing1_main.jpg", "drawing1_detail1.jpg"]
+        images: ["img/movement_01.png", "img/movement_02.png", "img/movement_03.png", "img/movement_04.png", "img/movement_05.png", "img/movement_06.png", "img/movement_07.png", "img/movement_02.png", "img/movement_03.png", "img/movement_04.png", "img/movement_05.png", "img/movement_06.png", "img/movement_07.png"]
     },
 
-    //**********/ Text - Statement
+    //**********/ Text - review
     {
-        id: "statement",
+        id: "review",
         nav: "text",
-        subnav: "statement",
+        subnav: "review",
         img: "",
         title: "howling 초안",
         subtitle: "플라니우스의 박물지 제35권에 부타데스의 딸의 일화가 나온 다. 그림의 기원에 관한 이야기라고도 한다. 기원전 600년 경 에 코린토스 지역의 도공 부타데스에게 딸이 있었다. 그녀에게 는 사랑하는 이가 있었는데 그녀에게서 멀리 떠나야 했다고 한 다. 그녀는 그 사람을 기억하기 위해 그 사람의 얼굴에 등불을 비추어 그림자를 그렸다. 그리고 그녀의 아버지는 그 그림자 위 에 부조를 얹는다. 그 얼굴은 기원전 146년경까지 보관되었다 고 한다.",
@@ -86,9 +106,9 @@ window.contents = [
         images: ""
     },
     {
-        id: "statement",
+        id: "review",
         nav: "text",
-        subnav: "statement",
+        subnav: "review",
         img: "",
         title: "howling 초안",
         subtitle: "플라니우스의 박물지 제35권에 부타데스의 딸의 일화가 나온 다. 그림의 기원에 관한 이야기라고도 한다. 기원전 600년 경 에 코린토스 지역의 도공 부타데스에게 딸이 있었다. 그녀에게 는 사랑하는 이가 있었는데 그녀에게서 멀리 떠나야 했다고 한 다. 그녀는 그 사람을 기억하기 위해 그 사람의 얼굴에 등불을 비추어 그림자를 그렸다. 그리고 그녀의 아버지는 그 그림자 위 에 부조를 얹는다. 그 얼굴은 기원전 146년경까지 보관되었다 고 한다.",
@@ -96,9 +116,9 @@ window.contents = [
         images: ""
     },
     {
-        id: "statement",
+        id: "review",
         nav: "text",
-        subnav: "statement",
+        subnav: "review",
         img: "",
         title: "howling 초안",
         subtitle: "플라니우스의 박물지 제35권에 부타데스의 딸의 일화가 나온 다. 그림의 기원에 관한 이야기라고도 한다. 기원전 600년 경 에 코린토스 지역의 도공 부타데스에게 딸이 있었다. 그녀에게 는 사랑하는 이가 있었는데 그녀에게서 멀리 떠나야 했다고 한 다. 그녀는 그 사람을 기억하기 위해 그 사람의 얼굴에 등불을 비추어 그림자를 그렸다. 그리고 그녀의 아버지는 그 그림자 위 에 부조를 얹는다. 그 얼굴은 기원전 146년경까지 보관되었다 고 한다.",
